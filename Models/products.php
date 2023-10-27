da8c131b2d3bdb14cd853f8407e174de8939ae23
Vivanco <76187493+vivanco-jhonny@users.noreply.github.com>
2023-10-27 17:05:36 -0500

    Delete Models directory

--- Models/products.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'database.php';
-class Producto extends Database {
-    function getProducts() {
-        $query = $this->conect()->query('SELECT * FROM products');
-        return $query;
-    }
-}
-?>
